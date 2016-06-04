f356a887390d3a9d8452f2dc2a6eaef65abaecf6
Mathias Holmbo <mathias.christensen@hotmail.com>
2016-06-04 16:44:06 +0200

    First commit - Design should be done for the app

---
+++ www/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope) {
+
+})
+.controller('AboutCtrl', function($scope) {
+
+})
+.controller('ContactCtrl', function($scope) {
+
+})
