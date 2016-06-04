f904bab59f947d14aaf5c6c281bf7e2b1e811f20
Casper Schultz <casper@Caspers-MacBook-Pro-2.local>
2016-06-04 22:54:53 +0200

    Added API calls

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,49 @@
+var baseUrl = 'http://api.casperschultz.info/wp-json/ron/v1/';
+
+
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function($scope, $http) {
+
+  $http({
+    method: 'GET',
+    url: baseUrl + 'menu'
+  }).then(function successCallback(response) {
+
+    console.log(response);
+
+    $scope.types = response.data;
+  });
 
 })
-.controller('AboutCtrl', function($scope) {
+
+.controller('AboutCtrl', function($scope, $http) {
+
+  $http({
+    method: 'GET',
+    url: baseUrl + 'about'
+  }).then(function successCallback(response) {
+    $scope.title = response.data.post_title;
+    $scope.body = response.data.post_content;
+  });
+
+  $http({
+    method: 'GET',
+    url: baseUrl + 'facebook'
+  }).then(function successCallback(response) {
+    $scope.picture = response.data.picture;
+    $scope.link = response.data.link;
+    $scope.feed = [];
+
+    response.data.posts.forEach(function(entry) {
+      if (entry.message) {
+        $scope.feed.push(entry);
+      }
+    });
+  });
 
 })
+
 .controller('ContactCtrl', function($scope) {
 
 })
--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,10 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+
+.filter('capitalize', function() {
+  return function(input) {
+    return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+  }
 });
